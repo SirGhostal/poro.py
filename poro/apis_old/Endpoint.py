fb275218cf5307d24c7508d3687466dc618503da
Zachy24 <Zachary.Howard@Outlook.com>
2018-12-21 21:45:21 +0000

    Remove old api

--- poro/apis_old/Endpoint.py
+++
@@ -1,8 +0,0 @@
-
-class Endpoint:
-    def __init__(self, http, url):
-        self.http = http
-        self.url = url
-
-    async def _raw_request(self, route):
-        return await self.http.request(route)
--- poro/apis_old/__init__.py
+++
@@ -1,7 +0,0 @@
-from .Http import HTTPClient
-from .ChampionApiV3 import ChampionApiV3
-
-__all__ = [
-    "HttpClient"
-    "ChampionApiV3"
-]
