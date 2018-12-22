b073cc21fc8957cd477a9a3f5387b95352e1e1d4
Zachy24 <Zachary.Howard@Outlook.com>
2018-12-22 01:19:49 +0000

    Add Setup.py, requirements.txt

---
+++ poro/__init__.py
@@ -0,0 +1,5 @@
+from .poro import Poro
+
+__all__ = [
+    "Poro"
+]
---
+++ setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+
+readme = ''
+with open('README.md') as f:
+    readme = f.read()
+
+requirements = []
+with open('requirements.txt') as f:
+  requirements = f.read().splitlines()
+
+setup(
+  name = 'poro.py',
+  packages = find_packages(),
+  license='MIT',
+  version = '1.0.0',
+  description = 'An async python framework for communicating with the Riot API',
+  long_description=readme,
+  author = 'Ghostal',
+  author_email = 'Zachary.Howard@outlook.com',
+  url = 'https://github.com/Zachy24/poro.py',
+  keywords = ['Riot API', 'python', 'poro'],
+  classifiers = [],
+  install_requires=requirements
+)
